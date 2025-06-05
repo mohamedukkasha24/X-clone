452cacd01c7f172152efd605367c2904929b7b71
mohamedukkasha24 <mohamedukkasha24@gmail.com>
2025-06-05 17:48:35 +0530

    updated

--- src/app/communities-x-layout/communities-x-layout.component.ts
+++ src/app/communities-x-layout/communities-x-layout.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { Router, RouterLink } from '@angular/router';
 @Component({
   selector: 'app-communities-x-layout',
   imports: [],
@@ -7,5 +7,64 @@ import { Component } from '@angular/core';
   styleUrl: './communities-x-layout.component.css'
 })
 export class CommunitiesXLayoutComponent {
+  constructor(
+    private homepage: Router,
+    private explorepage: Router,
+    private notificationpage: Router,
+    private messagepage: Router,
+    private bookmarkpage: Router,
+    private jobpage: Router,
+    private premiumpage: Router,
+    private verifiedorgspage: Router,
+    private profilepage: Router,
+    private listpage: Router,
+    private grokpage: Router,
+    private communitiespage: Router
+  ) {}
 
+  goToHome() {
+    this.homepage.navigate(['x-layout']);
+  }
+
+  goToExplore() {
+    this.explorepage.navigate(['explore']);
+  }
+
+  goToNotification() {
+    this.notificationpage.navigate(['notifications']);
+  }
+
+  goToMessage() {
+    this.messagepage.navigate(['messages']);
+  }
+
+  //private grokpage : Router
+  goToGrok() {
+    this.grokpage.navigate(['grok']);
+  }
+
+  goToBookmarks() {
+    this.bookmarkpage.navigate(['bookmarks']);
+  }
+  goToJobs() {
+    this.jobpage.navigate(['jobs']);
+  }
+
+  goToPremium() {
+    this.premiumpage.navigate(['premium']);
+  }
+
+  goToVerifiedOrgs() {
+    this.verifiedorgspage.navigate(['verifiedorgs']);
+  }
+  goToCommunities() {
+    this.communitiespage.navigate(['communities']);
+  }
+  goToProfile() {
+    this.profilepage.navigate(['profile']);
+  }
+
+  goToList() {
+    this.listpage.navigate(['list']);
+  }
 }
